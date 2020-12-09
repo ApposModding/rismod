34ff86c0f3884b5ea1ffd4dbec42aa6283a7ea3b
ApposModding <70633492+ApposModding@users.noreply.github.com>
2020-12-09 09:13:07 +0530

    readded WAILA

---
+++ src/main/java/org/souldbminer/reallyinspace/gui/overlay/WAILAOverlay.java
@@ -0,0 +1,93 @@
+
+package org.souldbminer.reallyinspace.gui.overlay;
+
+import org.souldbminer.reallyinspace.RisModElements;
+
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.energy.CapabilityEnergy;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.api.distmarker.Dist;
+
+import net.minecraft.world.World;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.client.Minecraft;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.platform.GlStateManager;
+
+@RisModElements.ModElement.Tag
+public class WAILAOverlay extends RisModElements.ModElement {
+	public WAILAOverlay(RisModElements instance) {
+		super(instance, 231);
+	}
+
+	@Override
+	public void initElements() {
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent(priority = EventPriority.NORMAL)
+	public void eventHandler(RenderGameOverlayEvent event) {
+		if (!event.isCancelable() && event.getType() == RenderGameOverlayEvent.ElementType.HELMET) {
+			int posX = (event.getWindow().getScaledWidth()) / 2;
+			int posY = (event.getWindow().getScaledHeight()) / 2;
+			PlayerEntity entity = Minecraft.getInstance().player;
+			World world = entity.world;
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			if (true) {
+				Minecraft.getInstance().fontRenderer.drawString("" + (new Object() {
+					public int getEnergyStored(BlockPos pos) {
+						AtomicInteger _retval = new AtomicInteger(0);
+						TileEntity _ent = world.getTileEntity(pos);
+						if (_ent != null)
+							_ent.getCapability(CapabilityEnergy.ENERGY, null).ifPresent(capability -> _retval.set(capability.getEnergyStored()));
+						return _retval.get();
+					}
+				}.getEnergyStored(new BlockPos((int) x, (int) y, (int) z))) + "", posX + 173, posY + -68, -16776961);
+				Minecraft.getInstance().fontRenderer.drawString("" + (new Object() {
+					public int getFluidTankLevel(BlockPos pos, int tank) {
+						AtomicInteger _retval = new AtomicInteger(0);
+						TileEntity _ent = world.getTileEntity(pos);
+						if (_ent != null)
+							_ent.getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null)
+									.ifPresent(capability -> _retval.set(capability.getFluidInTank(tank).getAmount()));
+						return _retval.get();
+					}
+				}.getFluidTankLevel(new BlockPos((int) x, (int) y, (int) z), 1)) + "", posX + 153, posY + -49, -65536);
+				RenderSystem.disableDepthTest();
+				RenderSystem.depthMask(false);
+				RenderSystem.blendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA,
+						GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+				RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+				RenderSystem.disableAlphaTest();
+				Minecraft.getInstance().getTextureManager().bindTexture(new ResourceLocation("ris:textures/modlogo.png"));
+				Minecraft.getInstance().ingameGUI.blit(posX + 193, posY + -106, 0, 0, 16, 16, 16, 16);
+				RenderSystem.depthMask(true);
+				RenderSystem.enableDepthTest();
+				RenderSystem.enableAlphaTest();
+				RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+				Minecraft.getInstance().fontRenderer.drawString("WAILA", posX + 183, posY + -85, -256);
+				Minecraft.getInstance().fontRenderer.drawString("" + (new Object() {
+					public String getValue(BlockPos pos, String tag) {
+						TileEntity tileEntity = world.getTileEntity(pos);
+						if (tileEntity != null)
+							return tileEntity.getTileData().getString(tag);
+						return "";
+					}
+				}.getValue(new BlockPos((int) x, (int) y, (int) z), "tagName")) + "", posX + 118, posY + -38, -65281);
+			}
+		}
+	}
+}
